169edbd954f398e8f947b267003d38ee6cadb89f
Bhagath Koraboina <Koraboinas@Bhagaths-MacBook-Pro.local>
2016-02-07 17:36:39 +0000

    my first git commit

--- src/Pack1/TestNG_Tests.java
+++ src/Pack1/TestNG_Tests.java
@@ -14,6 +14,7 @@ public class TestNG_Tests {
 	@BeforeMethod
 	public void openBrowser() {
 		driver = new FirefoxDriver();
+//testing git file
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
